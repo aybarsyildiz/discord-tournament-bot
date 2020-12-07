aa03518738ca622eeb01b35408bbbb2b9ff98c31
aybarsyildiz <aybarsyildizz@hotmail.com>
2020-12-07 20:56:27 +0300

    create tournament

--- commands/createtournament.js
+++ commands/createtournament.js
@@ -1,15 +1,27 @@
 const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
+const time = 10000
 module.exports = {
 
     name: 'createtournament',
     description: 'Create a tournament',
     execute(message){
         const queue = message.client.queue;
-        message.channel.send("Tournament has created! Press to :+1: play in a team!").then(sentEmbed => {
-            sleep (1000);
-            sentEmbed.react("👍");
+        message.channel.send("Tournament has created! Press to :+1: play in a team!");
+        message.react("👍");
 
-        })
+        const filter = (reaction, user) => {
+            return "👍";
+       };
+        const collector = message.createReactionCollector(filter, { time: time });
+        collector.on('collect', (reaction, reactionCollector) => {
+            let limit = 11;
+            if(reaction.count > limit){
+                reaction.message.delete();
+            }
+            console.log(reaction.users);
+            console.log("count is right here: %d",reaction.count);
+       });
+
         
 
     }
